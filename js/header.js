9313e18f1f46be6ed254d2b02501ac169200121b
matiasillaness <illanesmatias04@gmail.com>
2022-12-29 01:34:47 -0300

    arreglando bug header

--- js/header.js
+++ js/header.js
@@ -2,7 +2,7 @@ const btns = document.querySelector("#btn-mbl-nav");
 
 btns.addEventListener("click", (e) => {
   const btn = document.querySelector("#btn-mbl-nav button");
-  btn.classList.toggle('open')
+  btn.classList.toggle("open");
   let stylenav = document.querySelector("#header-main-list");
   let headerMain = document.querySelector(".header-main").style;
   if (stylenav.style.display === "none" || stylenav.style.display === "") {
@@ -10,11 +10,11 @@ btns.addEventListener("click", (e) => {
     headerMain.position = "sticky";
     headerMain.top = "0";
   } else {
-
-      stylenav.style.display = "none";
-      headerMain.position = "static";
+    stylenav.style.display = "none";
+    headerMain.position = "static";
   }
 });
+
 
 
 // we use a simple 1 pixel wide line
